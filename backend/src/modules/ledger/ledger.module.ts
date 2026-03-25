9e50e36bde94421f2b91d933aa21ca433a92a1ae
synteracloud <synteracloud@gmail.com>
2026-03-25 15:24:18 +0500

    Add bill.created ledger consumer with idempotent AP postings

--- backend/src/modules/ledger/ledger.module.ts
+++ backend/src/modules/ledger/ledger.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TransactionsModule } from '../../common/transactions/transactions.module';
 import { EventsModule } from '../events/events.module';
+import { EventQueueModule } from '../events/queue/event-queue.module';
+import { BillCreatedLedgerConsumer } from './bill-created-ledger.consumer';
 import { LedgerRepository } from './ledger.repository';
 import { LedgerService } from './ledger.service';
 
 @Module({
-  imports: [EventsModule, TransactionsModule],
-  providers: [LedgerRepository, LedgerService],
+  imports: [EventsModule, EventQueueModule, TransactionsModule],
+  providers: [LedgerRepository, LedgerService, BillCreatedLedgerConsumer],
   exports: [LedgerService]
 })
 export class LedgerModule {}
